08c2e9c4749120b1eeda09e5d1dbe794403c907f
audubilly <audubilly89@gmail.com>
2021-09-19 01:22:50 +0100

    spring security config

---
+++ src/main/java/com/billy/testdemo/config/SpringSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.billy.testdemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+         auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+         http.csrf().disable()
+                 .authorizeRequests()
+                 .antMatchers("/admin").hasRole("Admin")
+                 .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                 .and()
+                 .httpBasic();
+
+    }
+
+}
