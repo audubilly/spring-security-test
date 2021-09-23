d3d459fb1d13ec707fb7ecd1740f7186580e499c
audubilly <audubilly89@gmail.com>
2021-09-23 05:23:19 +0100

    refactored

--- src/main/java/com/billy/testdemo/config/JwtUtil.java
+++ src/main/java/com/billy/testdemo/config/JwtUtil.java
@@ -1,17 +1,15 @@
 package com.billy.testdemo.config;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.preauth.j2ee.J2eeBasedPreAuthenticatedWebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class JwtUtil {
@@ -29,26 +27,55 @@ public class JwtUtil {
         this.jwtExpirationInMs = jwtExpirationInMs;
     }
 
-    public String generateToken(UserDetails userDetails){
-        Map<String,Object> claims = new HashMap<>();
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
 
-       Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
-       if(roles.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
-           claims.put("isAdmin",true);
-       }
-        if(roles.contains(new SimpleGrantedAuthority("ROLE_USER"))){
-            claims.put("isUser",true);
+        Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
+        if (roles.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            claims.put("isAdmin", true);
         }
-        return doGenerateToken(claims,userDetails.getUsername());
+        if (roles.contains(new SimpleGrantedAuthority("ROLE_USER"))) {
+            claims.put("isUser", true);
         }
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
 
-        private String doGenerateToken(Map<String, Object> claims, String subject){
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
-                .signWith(SignatureAlgorithm.HS512,secret).compact();
-        }
-
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
+    public boolean validateToken(String authToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex){
+            throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        }
+    }
+
+    public String getUsernameFromToken(String token){
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody()
+        return claims.getSubject();
+    }
+
+    public List<SimpleGrantedAuthority> getRolesFromToken(String token){
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        List<SimpleGrantedAuthority> roles = null;
+        Boolean isAdmin = claims.get("isAdmin", Boolean.class);
+        Boolean isUser = claims.get("isUser", Boolean.class);
+
+        if (isAdmin != null && isAdmin){
+            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+        if (isUser != null && isAdmin){
+            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return roles;
+    }
+}
 
